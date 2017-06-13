4aa2d35dd5c96bf2d2d82e9bf720e2375e8ff51b
thiagoyves <thiagoyves@hotmail.com>
2017-06-12 22:55:10 -0300

    Os seguintes ajustes foram realizados:
    * nome do app foi corrigido
    * foi adicionada a chamda para o AutoComplete service do Google para buscar o endereco conforme preeenchimento por parte do usuario

--- app/src/main/java/com/ubicomp/tool4work/DisplayMessageActivity.java
+++ app/src/main/java/com/ubicomp/tool4work/DisplayMessageActivity.java
@@ -26,7 +26,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     private Event event;
     private Place current_place;
     private EditText editText_arrival_time;
-    private EditText editText_final_time;
+    private EditText editText_leave_time;
     private Switch switch_button;
     private Date arrival_date;
     private Date leave_date;
@@ -43,16 +43,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
 //        retrieve the data from the arrival and leave time fields from the view
         editText_arrival_time = (EditText) findViewById(R.id.arrival_time);
-        editText_final_time = (EditText) findViewById(R.id.leave_time);
+        editText_leave_time = (EditText) findViewById(R.id.leave_time);
         switch_button = (Switch) findViewById(R.id.switch_wifi);
 
         switch_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                     wifi.setWifiEnabled(true);
-                }
-                else {
+                } else {
                     wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                     wifi.setWifiEnabled(false);
                 }
@@ -60,6 +59,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
         });
 
         formatter = new SimpleDateFormat("hh:mm:ss a");
+
+        if (arrival_date != null && leave_date != null) {
+            Event new_event = new Event(current_place, arrival_date.getTime(), leave_date.getTime(), event_name);
+        }
 
         editText_arrival_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
@@ -69,51 +72,42 @@ public class DisplayMessageActivity extends AppCompatActivity {
                     try {
                         arrival_date = (Date)formatter.parse(arrival_time);
                     } catch (java.text.ParseException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                 }
             }
         });
 
-        editText_final_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        editText_leave_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
             public void onFocusChange(View v, boolean hasFocus) {
                 if(!hasFocus){
-                    String leave_time =  editText_final_time.getText().toString();
+                    String leave_time =  editText_leave_time.getText().toString();
                     try {
-                        arrival_date = (Date)formatter.parse(leave_time);
+                        leave_date = (Date)formatter.parse(leave_time);
                     } catch (java.text.ParseException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                 }
             }
         });
 
-        if(arrival_date != null &&  leave_date != null){
-            Event new_event = new Event(current_place, arrival_date.getTime(), leave_date.getTime(), event_name);
-        }
 
         PlaceAutocompleteFragment fragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
         fragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: obter informações sobre o local selecionado.
-
                 current_place = place;
-
                 Log.i("TAG", "Place: " + place.getName());
             }
 
             @Override
             public void onError(Status status) {
-                // TODO: Solucionar o erro.
                 Log.i("TAG", "Ocorreu um erro: " + status);
             }
         });
-
     }
 
 }
+
